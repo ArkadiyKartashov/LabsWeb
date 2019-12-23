0f51cfcb70b10561d22c9bb438751a9d5eb7d861
ArkadiyKartashov <arkasha-kartashov@yanedx.ru>
2019-12-23 14:06:31 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 
 int main()
 {
-    int a,b;
+    int a,b,c;
 
     printf("Enter numbers \n");
     printf("First number:");
     scanf("%d", &a);
     printf("Second number:");
     scanf("%d", &b);
+    c = a + b;
+    printf("Summ = %d", c);
+
 
 }
